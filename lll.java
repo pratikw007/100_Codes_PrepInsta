3ab3ff118678e8b4b3d4e07690b3367602fe712d
Emma Paris <eparis@atlassian.com>
2023-03-27 10:43:20 +0530

    Greatest of two numbers

---
+++ lll.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class lll {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Num1 = sc.nextInt();
+        int Num2 =sc.nextInt();
+
+        if(Num1<Num2){
+            System.out.println(Num2);
+        }
+        else{
+            System.out.println(Num1); 
+    }
+
+}
+}
