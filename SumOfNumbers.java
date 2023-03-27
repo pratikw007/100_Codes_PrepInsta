37006d72c9e22d5e20549a43f464e614a81b7ffd
Emma Paris <eparis@atlassian.com>
2023-03-27 10:28:16 +0530

    SumOFNumbers

---
+++ SumOfNumbers.java
@@ -0,0 +1,10 @@
+public class SumOfNumbers{
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum  += i;
+        }
+        System.out.println(sum);
+    }
+}
